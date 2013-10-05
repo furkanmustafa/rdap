ae8e796dd9fd5b9ec46b821e80d8449b6192c685
Furkan Mustafa <f@s2n.io>
2013-10-05 23:20:53 +0900

    Initial Commit

---
+++ rdap/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  rdap
+//
+//  Created by Furkan Mustafa on 10/5/13.
+//  Copyright (c) 2013 fume. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
